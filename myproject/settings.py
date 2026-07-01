0f9b85bb54e1192ef076f29828f012f93313d4a5
Joel Sunday <joelasheel@gmail.com>
2026-07-01 17:13:54 +0300

    fix allowed hosts for render

--- myproject/settings.py
+++ myproject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-zsd@551l3nbo1)fdrhipx4_&e##*_!f$=lve1of(^&so=i52i4
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.onrender.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
